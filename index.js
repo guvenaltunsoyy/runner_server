08639fcc52ffbb189b22e989ae73bd3d1a3c3579
Güven Altunsoy <guven@shopihq.com>
2019-12-24 18:19:14 +0300

    added log

--- index.js
+++ index.js
@@ -65,8 +65,8 @@ app.post('/runCount', function (req, res) {
 })
 
 app.post('/create', async function (req, res) {  
+  console.log(req.body.runner);
   const result = await dbHelper.insertRunnerImageTest(req.body.runner);
-  //console.log(req.body);
   //console.log("result => ", result);
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify({ result }, null, 3));
